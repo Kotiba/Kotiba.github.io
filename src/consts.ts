32d5de0643f01b221708a46d4e795c1ebdf87c0e
Kotiba <kotiba.marghani@gmail.com>
2025-01-25 02:40:28 +0200

    Changed meta data

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Atom - A Tailus template';
-export const SITE_DESCRIPTION = 'Modern free Tailwind CSS, Astro landing page template for startups | built with Tailus Themer';
-export const SITE_URL = 'https://atom.tailus.io';
+export const SITE_TITLE = 'MargStack';
+export const SITE_DESCRIPTION = 'Agency';
+export const SITE_URL = 'https://margstack.com';
 export const SITE_IMAGE = '/og.png';
